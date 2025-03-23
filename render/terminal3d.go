62c842f51b6395e15a6a0a8801023f579dce81d7
agioldas <aleksis.gioldaseas@outlook.com>
2025-03-23 03:15:53 +0200

    oups, wasn't in root directory. THIS IS JUST THE BEGINNING

--- render/terminal3d.go
+++ render/terminal3d.go
@@ -43,7 +43,7 @@ func NewCamera() camera {
 	camera := camera{}
 	camera.vAngle = 90
 	camera.hAngle = 90
-	camera.rayCount = 100
+	camera.rayCount = 200
 	camera.rayBoxSide = int(math.Sqrt(float64(camera.rayCount)))
 	camera.direction = DIRS.Forward
 
@@ -77,83 +77,168 @@ type world struct {
 
 //if you don't know what matrix multiplications are look em up, I suggest 3Blue1Brown
 
-// x  1      0      0
-// y  0   cosθ  −sinθ
-// z  0   sinθ   cosθ
+//  x      y      z
+//  1      0      0
+//  0   cosθ  −sinθ
+//  0   sinθ   cosθ
+
+// rotates vector in x axis
 func (p *pos) xRot(deg float64) {
 	rads := d2r(deg)
 	cos := math.Cos(rads)
 	sin := math.Sin(rads)
-	p.y = cos*p.y + -sin*p.y
-	p.z = sin*p.z + cos*p.z
+	newY := cos*p.y + -sin*p.z
+	p.z = sin*p.y + cos*p.z
+	p.y = newY
 }
 
-// x  cosθ   0  sinθ
-// y     0   1     0
-// z -sinθ   0  cosθ
+//     x   y     z
+//  cosθ   0  sinθ
+//     0   1     0
+// -sinθ   0  cosθ
+
+// rotates vector in y axis
 func (p *pos) yRot(deg float64) {
 	rads := d2r(deg)
 	cos := math.Cos(rads)
 	sin := math.Sin(rads)
-	p.x = cos*p.x + sin*p.x
-	p.z = -sin*p.z + cos*p.z
+	newX := cos*p.x + sin*p.z
+	p.z = -sin*p.x + cos*p.z
+	p.x = newX
 }
 
-// x cosθ -sinθ   0
-// y sinθ  cosθ   0
-// z    0     0   1
+//    x     y   z
+// cosθ -sinθ   0
+// sinθ  cosθ   0
+//    0     0   1
+
+// rotates vector in z axis
 func (p *pos) zRot(deg float64) {
 	rads := d2r(deg)
 	cos := math.Cos(rads)
 	sin := math.Sin(rads)
-	p.x = cos*p.x + -sin*p.x
-	p.y = sin*p.y + cos*p.y
+	newX := cos*p.x + -sin*p.y
+	p.y = sin*p.x + cos*p.y
+	p.x = newX
 }
 
-// turns degrees to rads
+// dot product
+func (p *pos) dot(p2 pos) float64 {
+	return p.x*p2.x + p.y*p2.y + p.z*p2.z
+}
+
+// multiply
+func (p *pos) mult(p2 pos) {
+	p.x *= p2.x
+	p.y *= p2.y
+	p.z *= p2.z
+}
+
+// addition
+func (p *pos) add(p2 pos) {
+	p.x += p2.x
+	p.y += p2.y
+	p.z += p2.z
+}
+
+// subtraction
+func (p *pos) sub(p2 pos) {
+	p.x -= p2.x
+	p.y -= p2.y
+	p.z -= p2.z
+}
+
+// normalizes the vector, basically makes the length of the vector to 1
+func (p *pos) norm() {
+	mag := p.mag()
+	p.x /= mag
+	p.y /= mag
+	p.z /= mag
+}
+
+// magnitude, basically the length of the vector
+func (p *pos) mag() float64 {
+	return math.Sqrt(p.x*p.x + p.y*p.y + p.z*p.z)
+}
+
+// makes the vector longer, meant to be used on normalized vectors
+func (p *pos) scale(scalar float64) {
+	p.x *= scalar
+	p.y *= scalar
+	p.z *= scalar
+}
+
+// turns degrees to radians
 func d2r(degrees float64) float64 {
 	return degrees * float64(math.Pi/180)
 }
 
-func (w *world) RenderFrame() {
-	// for y := range w.camera.rayBoxSide {
-	// 	for x := range w.camera.rayBoxSide {
+func (w *world) Fire(raycastPosition, raycastDirection pos, ball sphere) bool {
+	p := ball.center
+	p.sub(raycastPosition)
+	dotProduct := p.dot(raycastDirection)
 
-	// 	}
+	if dotProduct <= 0 {
+		return false
+	}
+
+	closestPointToCenter := raycastDirection
+	closestPointToCenter.scale(dotProduct)
+	closestPointToCenter.add(raycastPosition)
+
+	newV := ball.center
+	newV.sub(closestPointToCenter)
+
+	mag := newV.mag()
+	return mag <= ball.radius
+
+	// if equal(mag, ball.radius) {
+	// 	return true
 	// }
+
+	// return true
+}
+
+const epsilon = 1e-9
+
+func equal(a, b float64) bool {
+	return math.Abs(a-b) <= epsilon
+}
+
+func (w *world) RenderFrame() {
+
+	hSteps := w.camera.hAngle / float64(w.camera.rayBoxSide)
+	hStart := -(w.camera.vAngle / 2.0)
+	hEnd := -hStart
+
+	vSteps := w.camera.vAngle / float64(w.camera.rayBoxSide)
+	vStart := -(w.camera.vAngle / 2.0)
+	vEnd := -vStart
+	fmt.Println(hStart, hEnd, vStart, vEnd)
+	workingRaycast := pos{}
+	for h := hStart; h < hEnd; h += hSteps { //h stands for horizontal rotation
+		for v := vStart; v < vEnd; v += vSteps { //v stands for vertical rotation
+			workingRaycast = w.camera.direction
+			workingRaycast.zRot(h)
+			workingRaycast.yRot(v)
+			// fmt.Println(workingRaycast, h, v)
+			if w.Fire(w.camera.position, workingRaycast, w.objects[0]) {
+				fmt.Print("###")
+			} else {
+				fmt.Print(".-.")
+			}
+		}
+		fmt.Println()
+	}
 }
 
 func Test() {
-	ball := sphere{center: pos{x: 50, y: 0, z: 0}, radius: 10.0}
+	ball := sphere{center: pos{x: 50.0, y: 0, z: 0}, radius: 10.0}
 	camera := NewCamera()
 	myWorld := world{}
 	myWorld.objects = append(myWorld.objects, ball)
 	myWorld.camera = camera
 
-	p := pos{x: 60, y: 40, z: 20}
-	fmt.Println(p)
-	p.xRot(180)
-	fmt.Println(p)
-	p.xRot(180)
-
-	fmt.Println(p)
-	p.yRot(180)
-	fmt.Println(p)
-	p.yRot(180)
-
-	fmt.Println(p)
-	p.zRot(180)
-	fmt.Println(p)
-	p.zRot(180)
-	fmt.Println(p)
-
-	p.xRot(180)
-	p.yRot(180)
-	p.zRot(180)
-	fmt.Println(p)
-	p.xRot(180)
-	p.yRot(180)
-	p.zRot(180)
-	fmt.Println(p)
+	myWorld.RenderFrame()
 
 }
